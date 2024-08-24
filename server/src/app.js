2ef5c09b8a92f13cf717e634adc0ae95e1951b1e
Ansh <thebeliever39@gmail.com>
2024-08-24 11:44:57 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app=express();   
-const allowedOrigin = '*';
+const allowedOrigin = 'https://ecova.vercel.app';
 app.set("trust proxy", true);
 
 const corsOptions = {
